afac0d4e4d389fc109ef3bbc64cfeeb2e080acb3
Davirain <davirain.yin@gmail.com>
2025-11-02 16:26:36 +0800

    remvove

--- src/epub/mod.rs
+++
@@ -1,8 +0,0 @@
-mod model;
-mod service;
-
-pub use model::{
-    Book, BookContent, BookId, BookMetadata, Chapter, ChapterBlock, ManifestItem, Spine, TextSpan,
-    TocEntry,
-};
-pub use service::EpubService;
